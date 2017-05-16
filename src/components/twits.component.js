4b8ee4db1ebb383dc37f518aabe147a251605449
correju <correa.julian@gmail.com>
2017-05-15 19:32:28 -0500

    video added & styling

--- src/components/twits.component.js
+++ src/components/twits.component.js
@@ -5,8 +5,16 @@ export default {
   },
   template: `
     <section class="twits-wrapper" ng-repeat="twit in $ctrl.twits">
+      <div id="{{twit.id}}-video-wrapper">
+        <video data-setup='{ "techOrder": ["youtube"]' controls class="video-js vjs-default-skin" vjs-video></video>
+      </div>
       <twitter-widget twitter-widget-id="twit.id_str" twitter-widget-options="{'widget_type=': 'video', cards: 'sumary', 'align':'center', 'light': 'dark' }"></twitter-widget>
     </section>
   `,
   controller: 'twitsController'
 }
+
+
+//https://www.youtube.com/watch?v=eEHss0cNy9w
+
+// data-setup='{ "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=eEHss0cNy9w"}], "youtube": { "iv_load_policy": 1 } }'
