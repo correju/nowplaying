12b44b5ceda5d123512037a8597420b9f7cc0fe0
correju <correa.julian@gmail.com>
2017-05-17 09:53:23 -0500

    fix form styles

--- src/components/post.component.js
+++ src/components/post.component.js
@@ -7,19 +7,20 @@ export default {
     <section class="post-section">
       <form>
         <fieldset class="fieldset-container">
-            <div class="form-group  col-xs-5">
-              <input placeholder="Video Url" class="form-control" id="video-url" ng-model="$ctrl.url" type="text" />
+          <div class="flex-wrapper">
+              <div class="field-wrapper">
+                <input placeholder="Video Url" class="form-control" id="video-url" ng-model="$ctrl.url" type="text" />
+              </div>
+              <div class="field-wrapper">
+                <input placeholder="Message" id="message" class="form-control" ng-model="$ctrl.message" type="text" />
+              </div>
+              <div class="field-wrapper">
+                <button class="btn btn-primary form-group" ng-click="$ctrl.post()">
+                  <i class="fa fa-twitter"></i>
+                  Tweet to #nowPlaying
+                </button>
+              </div>
             </div>
-            <div class="form-group  col-xs-5">
-              <input class="form-control" placeholder="Message" id="message" ng-model="$ctrl.message" type="text" />
-            </div>
-            <div class="form-group  col-xs-2">
-              <button class="btn btn-primary form-group" ng-click="$ctrl.post()">
-                <i class="fa fa-twitter"></i>
-                Tweet to #nowPlaying
-              </button>
-            </div>
-
         </fieldset>
       </form>
     </section>
